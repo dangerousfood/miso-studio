5d557b846c46da931d58dc68ec3b117068cb76e3
omakasebar <omakasebar@gmail.com>
2021-05-18 23:35:38 -0500

    update youtube url

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,7 +34,7 @@
 				<vue-plyr :options="options">
 					<div class="plyr__video-embed">
 						<iframe
-							src="https://youtu.be/nwQkOn4wfEU"
+							src="https://www.youtube.com/watch?v=DVDh5ca4ta0"
 							allowfullscreen
 							allowtransparency
 						></iframe>
