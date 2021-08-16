371ce9462b224cbe5174b7c01f25df5f1d6ccc25
TopETH <supernathanliu@gmail.com>
2021-08-16 12:36:09 -0400

    fix: error from commit 0x0fefbe

--- app/pages/auctions/_address/index.vue
+++ app/pages/auctions/_address/index.vue
@@ -19,7 +19,23 @@
 						Region Restriction Detected
 					</h2>
 					<div
-						class="p-1 absolute top-0 right-0 w-5 h-5 rounded-full border-2 text-primary hover:text-high-emphesis border-primary d-flex items-center justify-content-center cursor-pointer"
+						class="
+							p-1
+							absolute
+							top-0
+							right-0
+							w-5
+							h-5
+							rounded-full
+							border-2
+							text-primary
+							hover:text-high-emphesis
+							border-primary
+							d-flex
+							items-center
+							justify-content-center
+							cursor-pointer
+						"
 						@click="closeModal"
 					>
 						<svg
@@ -289,6 +305,10 @@ export default {
 		},
 	},
 	async mounted() {
+		if (badAuctionsAddress.includes(this.auctionAddress)) {
+			this.$router.push('/')
+			return
+		}
 		await this.getTemplateId()
 		let type
 		// let finishAuction
